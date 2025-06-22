f7cf06c426239bc514cc656986c65d6add93e447
Jeffry <jeffry1408.job@gmail.com>
2025-06-22 16:45:08 +0700

    add another project

---
+++ 04-project-a-platform-game/js/drawingMethod.js
@@ -0,0 +1,93 @@
+const scale = 20;
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt("div", { class: "game" }, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  clear() {
+    this.dom.remove();
+  }
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let marginX = width / 3;
+  let marginY = height / 4;
+
+  // the viewport
+  let left = this.dom.scrollLeft,
+    right = left + width;
+  let top = this.dom.scrollTop,
+    bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+  if (center.x < left + marginX) {
+    this.dom.scrollLeft = center.x - marginX;
+  } else if (center.x > right - marginX) {
+    this.dom.scrollLeft = center.x + marginX - width;
+  }
+  if (center.y < top + marginY) {
+    this.dom.scrollTop = center.y - marginY;
+  } else if (center.y > bottom - marginY) {
+    this.dom.scrollTop = center.y + marginY - height;
+  }
+};
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${level.width * scale}px`,
+    },
+    ...level.rows.map((row) =>
+      elt(
+        "tr",
+        { style: `height: ${scale}px` },
+        ...row.map((type) => elt("td", { class: type })),
+      ),
+    ),
+  );
+}
+
+function drawActors(actors) {
+  return elt(
+    "div",
+    {},
+    ...actors.map((actor) => {
+      let rect = elt("div", { class: `actor ${actor.type}` });
+      rect.style.width = `${actor.size.x * scale}px`;
+      rect.style.height = `${actor.size.y * scale}px`;
+      rect.style.left = `${actor.pos.x * scale}px`;
+      rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    }),
+  );
+}
+
+export { elt, DOMDisplay, drawGrid, drawActors };
